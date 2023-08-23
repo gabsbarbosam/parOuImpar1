58130a48f7246f59cf686cf4175075d9db3edfcd
Gabriela Barbosa Martins <102072743+gabsbarbosam@users.noreply.github.com>
2023-08-22 21:25:55 -0300

    Add files via upload
    Par ou ímpar

--- script.js
+++ script.js
@@ -1,35 +1,9 @@
-function validatePassword() {
-    const passwordInput = document.getElementById("password");
-    const password = passwordInput.value;
-
-    const uppercaseRegex = /[A-Z]/;
-    const symbolRegex = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
-    
-    if (password.length < 8) {
-        displayErrorMessage("A senha deve ter pelo menos 8 caracteres.");
-        return false;
-    }
-
-    if (!uppercaseRegex.test(password)) {
-        displayErrorMessage("A senha deve conter pelo menos 1 letra maiúscula.");
-        return false;
-    }
-
-    if (!symbolRegex.test(password)) {
-        displayErrorMessage("A senha deve conter pelo menos 1 símbolo especial.");
-        return false;
-    }
-
-    hideErrorMessage();
-    return true;
+function verificarParImpar(numero) {
+  if (numero % 2 === 0) {
+    return "Par";
+  } else {
+    return "Ímpar";
+  }
 }
 
-function displayErrorMessage(message) {
-    const errorElement = document.getElementById("password-error");
-    errorElement.textContent = message;
-}
-
-function hideErrorMessage() {
-    const errorElement = document.getElementById("password-error");
-    errorElement.textContent = "";
-}
+console.log(verificarParImpar(7));
